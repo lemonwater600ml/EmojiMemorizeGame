eec04facbde99bdc8b158f38a67efe9e0e8ae9d6
Tienchi Hsin <thsin3@gatech.edu>
2020-12-17 01:34:37 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Hsin, Tienchi on 2020/12/17.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
